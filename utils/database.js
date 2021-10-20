92e784a70be22009100f83114af445d53e92a25c
unknown <z568sd232@gmail.com>
2021-10-20 21:01:06 +0800

    heroku

--- utils/database.js
+++ utils/database.js
@@ -21,9 +21,9 @@ if (isProduction) {
 });
 }
 
-/*pool.query('SELECT * from category_13', (err, res) => {
-  console.log(JSON.stringify(res.rows));
-  pool.end();
-});*/
+//pool.query('SELECT * from category_13', (err, res) => {
+// console.log(JSON.stringify(res.rows));
+//  pool.end();
+//});
 
 module.exports = pool;
