e33884e584784049420cd7d95ca0695ee5bfdaaf
unknown <z568sd232@gmail.com>
2021-10-20 22:22:01 +0800

    heroku

--- controllers/serviceCrown2Controller_13.js
+++ controllers/serviceCrown2Controller_13.js
@@ -1,8 +1,8 @@
-const category_13 = require('../models/Category_13');
+const Category_13 = require('../models/Category_13');
 
 exports.getCategories = async (req,res) => {
     try{
-        return await category_13.fetchA11();
+        return await Category_13.fetchAll();
     } catch (err){
         console.log('getCategories',err)
     }
--- models/Category_13.js
+++ models/Category_13.js
@@ -1,6 +1,6 @@
 const db = require('../utils/database');
 
-const category_13 = class category_13 {
+const Category_13 = class Category_13 {
     constructor(id,name,size,remote_url,local_url,link_url){
         this.id = id;
         this.name = name;
@@ -10,7 +10,7 @@ const category_13 = class category_13 {
         this.link_url = link_url;
     }
 
-    static async fetchA11() {
+    static async fetchAll() {
         try{
             let results = await db.query(`SELECT * from category_13`);
             return results.rows;
@@ -20,10 +20,10 @@ const category_13 = class category_13 {
     }
 };
 
-const test = async () => {
-    let results = await category_13.fetchA11();
-    console.log('results',JSON.stringify(results.rows));
-}
+//const test = async () => {
+//    let results = await category_13.fetchA11();
+//    console.log('results',JSON.stringify(results.rows));
+//}
 
-test();
-module.exports = category_13;
+//test();
+module.exports = Category_13;
